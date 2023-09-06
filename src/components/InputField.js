1cec1545fbba9dfeca0c133e658559de58f8416a
Rahul Jadhav <jadhav.rj.cmos@gmail.com>
2023-09-06 22:19:52 +0530

    Update InputField.js

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -3,11 +3,11 @@ import '../styles/App.css';
 
 //complete the following using forward refs concept 
 
-const InputField=forwardRef((props, ref)=>{
-    return(
+const InputField=forwardRef((props, ref) => {
+  return (
     <div>
-      <input id="input" type="text"  ref={ref} {...props}/>
+      <input id="input" type="text" ref={ref} {...props} />
     </div>
-    )
-    )}
+  );
+});
 export default InputField;
